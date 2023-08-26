4ff2c7c19d5f301695a4b98011bb2f79772ebc63
Performer <ghostmalek@hotmail.com>
2023-08-26 22:40:30 +0100

    Update Breakers Preset Manager.bat
    -Modified the Loading system to use the folder numbers instead of typing the full name of the preset.
    
    -Modified display to only show  showing the preset name instead of the folder name.
    
    -Fixed a bug when trying to load a preset and overwrite that same preset at the same time, Now loads the old one first but overwrites it with your current preset after it finishes loading.

--- Breakers Preset Manager.bat
+++ Breakers Preset Manager.bat
@@ -1,19 +1,22 @@
 @echo off
-:Check
-rem Check if Breakers is running, if it is, exit the script. Continue if not.
-SET MyProcess=DRAGON BALL THE BREAKERS.exe
-TASKLIST | FINDSTR /I "%MyProcess%")
-IF ERRORLEVEL 1 (echo This script is made by MrPan, to make it easier to manage different character presets in Dragon Ball The Breakers, it only copies and renames existing save directories for you, please close the game before using this.) ELSE (GOTO :ERROR)
-:List
-setlocal
+:Message
 
+echo This script is made by MrPan, to make it easier to manage different character presets in Dragon Ball The Breakers, it only copies and renames existing save directories for you, it is highly recommended to have Dragon Ball the Breakers closed before using this. If you would still like to use this in-game, it is required to be on the title screen level the moment you run the script.
+
+setlocal enabledelayedexpansion
 rem change to the Bandai Namco Entertainment directory to scout for existing presets
 cd /d %LocalAppData%\BANDAI NAMCO ENTERTAINMENT\
 
 rem List the presets with the proper BPM naming format 
 echo Existing presets found:
-dir /b | FIND "DRAGON BALL THE BREAKERS -" 
-endlocal
+set /a ID=1
+
+for /D %%G in ("DRAGON BALL THE BREAKERS - *") do (
+    set f=%%~G
+    set filename[!ID!]=!f:~27!
+    echo [!ID!] !f:~27!
+    set /a ID+=1
+)
 :START
 choice /C:sld /M "Would you like to [S]ave your current preset, [L]oad an existing preset or [D]elete a preset?"
 if %ERRORLEVEL% EQU 1 (GOTO :SAVE) 
@@ -32,19 +35,21 @@ xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" "%Loc
 msg * "Preset successfully saved to DRAGON BALL THE BREAKERS - %saven%."
 exit
 :LOAD
-set /p "preset=Which preset name would you like to load?" 
+set /p "n=Please type the number of the preset you'd like to load: " 
+set preset=!filename[%n%]!
+echo %preset% selected.
 if exist "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %preset%" (choice /C:yn /M "Would you like to save your current preset folder?") else (GOTO :NOTFOUND)
 if %ERRORLEVEL% EQU 1 (set /p "savename=Please type a name for your current preset to save:" ) else (GOTO :LOADPROCESS)
 set %ERRORLEVEL%= 0
 if defined savename if exist "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %savename%" (choice /C:yn /M "Would you like to overwrite the existing preset %savename% ?")
-if %ERRORLEVEL% EQU 1 (xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %savename%" /c /h /e /i /Y && msg * "Saved current preset to DRAGON BALL THE BREAKERS - %savename%." ) else (if %ERRORLEVEL% EQU 2 (xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %savename% (Copy)" /c /h /e /i /Y  && msg * "Saved current preset to DRAGON BALL THE BREAKERS - %savename% (Copy)."))
+if %ERRORLEVEL% EQU 1 (xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - temp" /c /h /e /i /Y && rmdir /q /s "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" &&  xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %preset%" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" /c /h /e /i /Y && rmdir /q /s "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %preset%" && xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - temp" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %savename%" /c /h /e /i /Y && rmdir /q /s "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - temp" && msg * "Saved current preset to DRAGON BALL THE BREAKERS - %savename%." && exit) else (if %ERRORLEVEL% EQU 2 (xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %savename% (Copy)" /c /h /e /i /Y  && msg * "Saved current preset to DRAGON BALL THE BREAKERS - %savename% (Copy)."))
 :LOADPROCESS
 rmdir /q /s "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" 
 xcopy "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %preset%" "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS" /c /h /e /i /Y
 if defined savename (msg * "Preset "DRAGON BALL THE BREAKERS - %preset%" loaded successfully, old preset folder saved to "DRAGON BALL THE BREAKERS - %savename%".") else (msg * "Preset "DRAGON BALL THE BREAKERS - %preset%" loaded successfully.") 
 exit
 :DELETE
-set /p "presetd= Which preset name would you like to delete? WARNING: THE FOLDER WILL BE GONE. " 
+set /p "presetd= Type the name of the preset which you'd like to be deleted. WARNING: THE FOLDER WILL BE GONE : " 
 if exist "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %presetd%" (rmdir /q /s "%LocalAppData%\BANDAI NAMCO ENTERTAINMENT\DRAGON BALL THE BREAKERS - %presetd%" & msg * "Preset DRAGON BALL THE BREAKERS - %presetd% deleted successfully.") else (GOTO :NOTFOUNDD)
 exit
 :ERROR
@@ -59,5 +64,6 @@ exit
 :NOTFOUNDD
 msg * "Preset "DRAGON BALL THE BREAKERS - %presetd%" not found."
 exit
+endlocal
 
 
